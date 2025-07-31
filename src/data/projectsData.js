1d572b7bfff5acfccbf36f2f78c2badb0cdaec51
Esther Babaoye <estherbabaoye@gmail.com>
2025-07-31 08:44:58 +0100

    url fix

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -4,7 +4,7 @@ const projects = [
     title: "PowerOrg – Renewable Energy Website",
     description:
       "A responsive site built for PowerOrg to showcase scalable solar and wind energy solutions. Designed to attract enterprise clients with clear CTAs and a clean UI.",
-    image: "/images/powerorg.png", // rename your uploaded screenshot to this and place in /public/images
+    image: "/images/powerorg.png", 
     link: "https://powerorg.netlify.app/",
   },
   {
@@ -12,8 +12,8 @@ const projects = [
     title: "URBank – Digital Banking Platform",
     description:
       "URBank is a user-first digital banking interface built for simplicity, trust, and control. From tailored account options to secure mobile access, it empowers users to manage money confidently.",
-    image: "/images/urbank.png", // rename your second screenshot to this and place in /public/images
-    link: "https://urbank.netlify.app/",
+    image: "/images/urbank.png", 
+    link: "https://urbank.nhsurulere.site/",
   },
 ];
 
